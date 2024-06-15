c5bfd01921785c1c12e792866f4c79335abfdb45
kaushal jalal <abhisheksingh2239@gmail.com>
2024-06-15 17:06:25 +0530

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import { useState } from "react";
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Blog from "./pages/Blog";
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+        </ul>
+      </nav>
 
-  return <>App</>;
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
